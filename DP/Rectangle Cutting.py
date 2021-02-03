e20ce1314350632cd286a1828bb3f8f1c1f73115
kkgarai <kkgarai99@gmail.com>
2021-02-04 00:22:22 +0530

    Add Rectangle Cutting

--- DP/Rectangle Cutting.py
+++ DP/Rectangle Cutting.py
@@ -19,3 +19,20 @@ Input:
 Output:
 3
 """
+
+
+a, b = list(map(int, input().split()))
+
+dp = [[float('inf') for _ in range(b+1)] for _ in range(a+1)]
+for i in range(a+1):
+    for j in range(b+1):
+        if i == j:
+            dp[i][j] = 0
+        else:
+            for k in range(1, i):
+                dp[i][j] = min(dp[i][j], 1+dp[k][j]+dp[i-k][j])
+
+            for k in range(1, j):
+                dp[i][j] = min(dp[i][j], 1+dp[i][k]+dp[i][j-k])
+
+print(dp[-1][-1])
